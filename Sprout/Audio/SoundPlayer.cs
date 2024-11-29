d6d57c97da13bc3281a5388bb2f9d742740efaa3
Ene <masqufeik@gmail.com>
2024-11-29 16:22:04 -0300

    Add SetHzPassFilter() for High/Low pass filters

--- Sprout/Audio/SoundPlayer.cs
+++ Sprout/Audio/SoundPlayer.cs
@@ -1,6 +1,8 @@
 using ManagedBass;
 using ManagedBass.Fx;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Threading.Channels;
 
 namespace Sprout.Audio;
 
@@ -9,10 +11,19 @@ namespace Sprout.Audio;
 /// </summary>
 public class SoundPlayer : IDisposable
 {
+    public enum PassFilter
+    {
+        High,
+        Low,
+        None,
+    }
+
     private bool _isDisposed, _isPlaying; // States.
-    private int _streamHandle = -1; // Handle.
+    private int _streamHandle = -1, _filterHandle; // Handles.
     private float _volume = 1, _panning = 0, _speed = 0, _pitch = 0; // Channel attributes.
-    private int? _highPassFilter = null, _lowPassFilter = null; // Hz pass.
+    private double _filterPercentage = 0;
+    private PassFilter _savedFilter = PassFilter.None;
+    private SampleInfo _sampleInfo = new();
     private bool _isSettingsGlobal = true;
 
     /// <summary>
@@ -73,20 +84,16 @@ public class SoundPlayer : IDisposable
     {
         get
         {
-            if (_streamHandle != 0)
-            {
-                // get lenght
-                long lengthInBytes = Bass.ChannelGetLength(_streamHandle);
-
-                // convert byte length to seconds
-                double lengthInSeconds = Bass.ChannelBytes2Seconds(_streamHandle, lengthInBytes);
-
-                return lengthInSeconds;
-            }
-            else
-            {
+            if (_streamHandle == 0)
                 return 0;
-            }
+
+            // get lenght
+            long lengthInBytes = Bass.ChannelGetLength(_streamHandle);
+
+            // convert byte length to seconds
+            double lengthInSeconds = Bass.ChannelBytes2Seconds(_streamHandle, lengthInBytes);
+
+            return lengthInSeconds;
         }
     }
 
@@ -115,6 +122,11 @@ public class SoundPlayer : IDisposable
             Bass.StreamFree(_streamHandle);
         }
 
+        if (_filterHandle != 0) // remove previous effects.
+        {
+            RemoveFilter();
+        }
+
         try
         {
             // create stream from filepath.
@@ -126,16 +138,22 @@ public class SoundPlayer : IDisposable
             if (_streamHandle == 0)
                 throw new InvalidOperationException("Failed to create stream: " + Bass.LastError);
 
+            // get audio info and save it.
+            Bass.SampleGetInfo(_streamHandle, _sampleInfo);
+
+            // if global settings are enabled, then the new audio will have the previous settings.
+            if (_isSettingsGlobal)
+            {
+                SetGlobalAttributes();
+                SetHzPassFilter(_savedFilter, _filterPercentage);
+                Debug.WriteLine($"Filters applied at playback: f:{_savedFilter} p:{_filterPercentage}");
+            }
+
             // start playback
             if (!Bass.ChannelPlay(_streamHandle))
                 throw new InvalidOperationException("Failed to play stream: " + Bass.LastError);
 
             _isPlaying = true;
-
-            if (_isSettingsGlobal)
-            {
-                SetGlobalAttributes();
-            }
 
             // playback ended event.
             Bass.ChannelSetSync(_streamHandle, SyncFlags.End, 0, (handle, channel, data, user) =>
@@ -144,12 +162,14 @@ public class SoundPlayer : IDisposable
 
                 _isPlaying = false;
 
-                Debug.WriteLine($"[SoundPlayer] Playback ended for: {filePath}");
+                RemoveFilter();
                 Bass.StreamFree(_streamHandle); // free the stream
                 _streamHandle = 0; // reset the stream handle
+
+                Debug.WriteLine($"[SoundPlayer] Playback ended");
             });
 
-            Debug.WriteLine("[SoundPlayer] Playing sound");
+            Debug.WriteLine($"[SoundPlayer] Playing sound: {filePath}");
         }
         catch (Exception ex)
         {
@@ -162,11 +182,11 @@ public class SoundPlayer : IDisposable
     /// </summary>
     public void Stop()
     {
-        if (_streamHandle != 0)
-        {
-            Bass.ChannelStop(_streamHandle);
-            Debug.WriteLine("[SoundPlayer] Sound stopped.");
-        }
+        if (_streamHandle == 0)
+            return;
+
+        Bass.ChannelStop(_streamHandle);
+        Debug.WriteLine("[SoundPlayer] Sound stopped.");
     }
 
     /// <summary>
@@ -174,11 +194,11 @@ public class SoundPlayer : IDisposable
     /// </summary>
     public void Pause()
     {
-        if (_streamHandle != 0)
-        {
-            Bass.ChannelPause(_streamHandle);
-            Debug.WriteLine("[SoundPlayer] Sound paused.");
-        }
+        if (_streamHandle == 0)
+            return;
+
+        Bass.ChannelPause(_streamHandle);
+        Debug.WriteLine("[SoundPlayer] Sound paused.");
     }
 
     /// <summary>
@@ -187,11 +207,11 @@ public class SoundPlayer : IDisposable
     /// <param name="miliseconds"></param>
     public void SetPosition(int miliseconds)
     {
-        if (_streamHandle != 0)
-        {
-            long newPos = Bass.ChannelSeconds2Bytes(_streamHandle, miliseconds / 1000);
-            Bass.ChannelSetPosition(_streamHandle, newPos);
-        }
+        if (_streamHandle == 0)
+            return;
+
+        long newPos = Bass.ChannelSeconds2Bytes(_streamHandle, miliseconds / 1000);
+        Bass.ChannelSetPosition(_streamHandle, newPos);
     }
 
     public void SetVolume(float volume) => SetAttribute(_streamHandle, ChannelAttribute.Volume, volume);
@@ -204,28 +224,26 @@ public class SoundPlayer : IDisposable
 
     private void SetAttribute(int handle, ChannelAttribute attribute, float value)
     {
-        if (_streamHandle != 0)
+        if (_streamHandle == 0)
             return;
 
         Bass.ChannelSetAttribute(_streamHandle, attribute, value);
 
-        if (attribute == ChannelAttribute.Volume)
+        switch (attribute)
         {
-            _volume = value;
+            case ChannelAttribute.Volume:
+                _volume = value;
+                break;
+            case ChannelAttribute.Pan:
+                _panning = value;
+                break;
+            case ChannelAttribute.Tempo:
+                _speed = value;
+                break;
+            case ChannelAttribute.Pitch:
+                _pitch = value;
+                break;
         }
-        else if (attribute == ChannelAttribute.Pan)
-        {
-            _panning = value;
-        }
-        else if (attribute == ChannelAttribute.Tempo)
-        {
-            _speed = value;
-        }
-        else if (attribute == ChannelAttribute.Pitch)
-        {
-            _pitch = value;
-        }
-
     }
 
     private void SetGlobalAttributes()
@@ -242,15 +260,95 @@ public class SoundPlayer : IDisposable
     /// <returns></returns>
     public double GetPosition()
     {
-        if (_streamHandle != 0)
-        {
-            long posInBytes = Bass.ChannelGetPosition(_streamHandle);
-            double posInSec = Bass.ChannelBytes2Seconds(_streamHandle, posInBytes);
+        if (_streamHandle == 0)
+            return 0;
 
-            return posInSec;
+        long posInBytes = Bass.ChannelGetPosition(_streamHandle);
+        double posInSec = Bass.ChannelBytes2Seconds(_streamHandle, posInBytes);
+
+        return posInSec;
+    }
+
+    /// <summary>
+    /// Removes all FX filters applied.
+    /// </summary>
+    private void RemoveFilter()
+    {
+        if (_streamHandle == 0)
+            return;
+
+        Bass.ChannelRemoveFX(_streamHandle, _filterHandle);
+        _filterHandle = 0;
+
+        Debug.WriteLine($"[SoundPlayer] Removing filters");
+    }
+
+    private float CalculateFrequency(double maxFreq, double percentage)
+    {
+        return (float)(maxFreq * percentage / 100); // rule of three based on the max sample rate from the file when an usuable percentage is given.
+    }
+
+    private BQFType GetBQFType(PassFilter filter)
+    {
+        return filter switch
+        {
+            PassFilter.Low => BQFType.LowPass,
+            PassFilter.High => BQFType.HighPass,
+            _ => throw new ArgumentOutOfRangeException(nameof(filter), "Invalid filter type")
+        };
+    }
+
+    public void SetHzPassFilter(PassFilter type, double percentage = 0)
+    {
+        if (_streamHandle == 0)
+            return;
+
+        // keep percentage on a valid range
+        percentage = Math.Clamp(percentage, 0, 99);
+
+        // save values given.
+        _filterPercentage = percentage;
+        _savedFilter = type;
+
+        // get the maximum Hz availible from sample rate divided by 2.
+        double maxFreq = _sampleInfo.Frequency / 2;
+
+        // if no filter is selected, then just remove it.
+        if (_savedFilter == PassFilter.None)
+        {
+            RemoveFilter();
+            return;
         }
 
-        return 0;
+        // TODO: for some reason, 1Hz should be "the default", but it bugs the audio, so for now we will just remove effects when 0% is selected.
+        if (percentage <= 0)
+        {
+            RemoveFilter();
+            return;
+        }
+
+        float freq = CalculateFrequency(maxFreq, percentage);
+
+        BQFParameters parameters = new()
+        {
+            fCenter = freq, // cut-off frequency for LowPass or HighPass.
+            lFilter = GetBQFType(type)
+        };
+
+        if (_filterHandle == 0) // create filter if it doesnt exist.
+        {
+            Debug.WriteLine($"[SoundPlayer] Filter didnt exist, creating...");
+            _filterHandle = Bass.ChannelSetFX(_streamHandle, EffectType.BQF, 0);
+
+            if (_filterHandle == 0)
+                throw new InvalidOperationException($"Failed to set {type} filter: {Bass.LastError}");
+        }
+
+        // apply the filter parameters
+        if (!Bass.FXSetParameters(_filterHandle, parameters))
+            throw new InvalidOperationException($"Failed to apply parameters for {type} filter: {Bass.LastError}");
+
+        Debug.WriteLine($"[SoundPlayer] {type} filter applied: Frequency={freq}");
     }
 
     /// <summary>
@@ -258,13 +356,19 @@ public class SoundPlayer : IDisposable
     /// </summary>
     public void Dispose()
     {
-        if (!_isDisposed && _streamHandle != 0)
+        if (_streamHandle == 0)
+            return;
+
+        if (!_isDisposed)
         {
+            RemoveFilter();
+
             Bass.ChannelStop(_streamHandle); // Stop the stream if its playing
             Bass.Free(); // Free all BASS resources
             _streamHandle = 0; // Clear the stream handle
 
             _isDisposed = true;
+
             Debug.WriteLine("[SoundPlayer] BASS resources freed.");
         }
     }
