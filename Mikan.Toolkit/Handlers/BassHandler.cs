8bf73d6d91d1d801eebc3710afe0f68334f078ab
ene02 <masqufeik@gmail.com>
2024-12-16 15:10:47 -0300

    Add handlers for SDL and Bass.

---
+++ Mikan.Toolkit/Handlers/BassHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagedBass;
+
+namespace Mikan.Toolkit.Handlers
+{
+    internal static class BassHandler
+    {
+        private static bool _isBASSInitialized = false;
+
+        public static bool IsBASSInitialized => _isBASSInitialized;
+        public static void CheckBassInit()
+        {
+            if (!Bass.Init())
+            {
+                if (Bass.LastError == Errors.Already)
+                {
+                    Debug.WriteLine($"[ManagedBass] BASS already initialized");
+                    return;
+                }
+
+                Debug.WriteLine($"[ManagedBass] Failed to initialize BASS: {Bass.LastError}");
+            }
+
+            Debug.WriteLine($"[ManagedBass] BASS initialized successfully");
+            _isBASSInitialized = true;
+
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                EndBass();
+            };
+        }
+
+        public static void EndBass()
+        {
+            if (!_isBASSInitialized)
+                return;
+
+            Bass.Free();
+            _isBASSInitialized = false;
+            Debug.WriteLine($"[ManagedBass] BASS resources were freed");
+        }
+    }
+}
---
+++ Mikan.Toolkit/Handlers/SDLHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SDL2.SDL;
+
+namespace Mikan.Toolkit.Handlers;
+
+public static class SDLHandler
+{
+    private static bool _isSDLInitialized = false;
+
+    public static bool IsSDLInitialized => _isSDLInitialized;
+
+    public static void CheckSDLInit()
+    {
+        if (_isSDLInitialized)
+            return;
+
+        // Initialize SDL.
+        if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        {
+            Debug.WriteLine($"[SDL] Could not initialize! SDL_Error: {SDL_GetError()}");
+            return;
+        }
+
+        Debug.WriteLine($"[SDL] Initiated!");
+        _isSDLInitialized = true;
+
+        AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+        {
+            EndSDL();
+        };
+    }
+
+    public static void EndSDL()
+    {
+        if (_isSDLInitialized)
+            SDL_Quit();
+    }
+}
